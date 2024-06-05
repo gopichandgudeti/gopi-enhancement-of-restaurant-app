353ab9c51e6acab0b6a22b94675120a6239101c9
gopichandgudeti <chandgudeti@gmail.com>
2024-06-05 05:03:21 +0000

    solution

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ const Header = props => {
   const {cartItems} = props
 
   return (
-    <nav className="header-bg-container">
+    <nav className='header-bg-container'>
       <h1>UNI Resto Cafe</h1>
       <p>My Orders {cartItems.length}</p>
     </nav>
